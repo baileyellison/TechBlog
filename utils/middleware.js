80de57b32d6ea16e5ab8040e9b6179963b995cc2
baileyellison <beeusc2018@gmail.com>
2024-06-29 22:34:22 -0400

    added middleware and helpers

---
+++ utils/middleware.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('../config/connection');
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Session middleware
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}));
+
+// Authentication middleware
+const withAuth = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    console.log('User is logged in:', req.session.user_id); // Debugging line
+    next();
+  }
+};
+
+module.exports = withAuth;
